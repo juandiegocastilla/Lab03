861aeb98eea1ff7c5cf6cf9a553e13b47830e536
juandiegocastilla <est.juan.castilla@unimilitar.edu.co>
2024-09-18 15:23:55 -0500

    Configuracion incial de cronoemtro

--- src/main/java/Ej1/Cronometro.java
+++ src/main/java/Ej1/Cronometro.java
@@ -4,11 +4,36 @@
  */
 package Ej1;
 
+import java.util.Scanner;
+import java.util.Timer;
+
 
 /**
  *
  * @author juand
  */
 public class Cronometro {
- 
+ private static int horas = 0;
+    private static int minutos = 0;
+    private static int segundos = 0;
+    //timer se usaria como objeto si se declarara en un archivo aparte, sin enmbargo ya que esta en el mismo archovo no es necesario usar un objeto 
+    private static Timer timer = new Timer();
+    private static boolean alarmaActiva = false;
+    private static boolean runn = true;
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Configura el tiempo de la alarma en minutos:\n(para detener el cronometro escriba 1 en consola)\n ");
+        int tiempoAlarma = scanner.nextInt();
+        
+
+        iniciarCronometro(tiempoAlarma);
+        while (runn) {
+            String input = scanner.next();
+            if (input.equals("1")) {
+                pararCronometro();
+            }
+        }
+        scanner.close();
+    }
 }
